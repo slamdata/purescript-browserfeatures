43d424f1019a5c788ed940cc4bcc9c0796ce236a
Jonathan Sterling <jon@jonmsterling.com>
2015-09-04 12:09:03 -0700

    [PR Feedback] use `for` instead of `flip traverse`
    thanks @cryogenian!

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Console (CONSOLE(), log)
 
-import Data.Traversable (traverse)
+import Data.Traversable (traverse, for)
 import qualified Data.BrowserFeatures.InputType as IT
 import DOM
 import DOM.BrowserFeatures.Detectors
@@ -12,5 +12,5 @@ import DOM.BrowserFeatures.Detectors
 main :: Eff (dom :: DOM, console :: CONSOLE) Unit
 main = do
   features <- detectBrowserFeatures
-  void $ flip traverse IT.allInputTypes \ty ->
+  void $ for IT.allInputTypes \ty ->
     log $ show ty ++ if features.inputTypeSupported ty then " supported" else " not supported"
